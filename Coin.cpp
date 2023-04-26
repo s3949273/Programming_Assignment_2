9ee3f40f599e85ff08eeb1a293b234da7b1c796b
s3949273 <s3949273@student.rmit.edu.au>
2023-04-26 16:28:02 +1000

    added a somewhat functional constructor for the coin object

--- Coin.cpp
+++ Coin.cpp
@@ -1,3 +1,22 @@
 #include "Coin.h"
- 
+#include <iostream>
+using std::vector; 
  // implement functions for managing coins; this may depend on your design.
+Coin::Coin(unsigned cents, unsigned amount){
+    vector<int> denominations{5,10,20,50, 100, 200, 500, 1000};
+    size_t index = 7;
+    bool denom_found = false;   
+    while (!denom_found && index >=0){
+        if(cents % denominations[index]==0){
+            //use of static_cast referenced from: https://stackoverflow.com/questions/321801/enum-c-get-by-index 
+            this->denom = static_cast<Denomination>(index);
+            denom_found = true;
+        }
+        else{
+            index -=1;
+        }
+    }
+    this->count = amount;
+
+
+}
