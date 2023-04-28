a2825d6da1d0dee3697cffa6131bc831cbc30ba7
s3949273 <s3949273@student.rmit.edu.au>
2023-04-28 21:41:37 +1000

    did some work on the main_menu_input() method, which now handles the first input of the user

--- ppd.cpp
+++ ppd.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <dirent.h>
 #include "LinkedList.h"
 #include "helper.h"
 #include <fstream> 
 #include <string>
 
-#define TAB "   " 
-
+#define TAB "   "
 using std::cout;
 using std::cin;
 using std::endl;
@@ -24,7 +24,7 @@ void print_main_menu(){
     cout<<TAB<<"1. Display Items"<<endl;
     cout<<TAB<<"2. Display Items"<<endl;
     cout<<TAB<<"3. Display Items"<<endl;
-    cout<<"Administrator-Only Menu";
+    cout<<"Administrator-Only Menu"<<endl;
     cout<<TAB<<"4. Add Item"<<endl;
     cout<<TAB<<"5. Remove Item"<<endl;
     cout<<TAB<<"6. Display Coins"<<endl;
@@ -42,46 +42,88 @@ int main_menu_input(){
     while (!valid_input){
         print_main_menu();
         string input = Helper::readInput();
+        
         try{
-            if (ret >=1 && ret <=9){
+            if (cin.eof()){
+                throw std::invalid_argument("Input was EOF character");
+                
+            }
+            try{
+                ret = stoi(input);
+                if (ret >=1 && ret <=9){
                 valid_input = true;
-            }else{
-                throw std::invalid_argument("Input was not between 1-9");
+                }else{
+                    throw std::out_of_range("");
+                }
+            }catch(std::invalid_argument& e){
+                cout<<"you tried to enter a letter"<<endl;
+            }
+            catch(std::out_of_range& e){
+                cout<<"Input was not between 1-9"<<endl;
             }
         }
-        catch(std::exception& e){
+        catch(std::invalid_argument& e){
             cout << e.what() << "\n" << endl;
+            ret = 9;
+            valid_input = true;
         }
+    
     }
     return ret;
 }
-
 
 
 int main(int argc, char **argv)
 {
     /* validating command line arguments */
     if (argc != 3) {throw std::invalid_argument("invalid number of arguments");}
-
+    LinkedList* all_stock = new LinkedList();
     /* read stock file */
-    // try{
-    //     string stock_data = string(argv[1]);
-    //     ifstream stocks(stock_data);
-    //     string line_stock;
-    //     vector<string> output_stock;
-    //     size_t counter = 0;
-    //     while (getline(stocks, line_stock)){
-    //         Helper::splitString(line_stock, output_stock, "|");
-    //         // cout << line_stock << endl;
-    //         output_stock.at(counter);
-    //         counter++;
-    //     }
-    //     stocks.close();
-    // }catch(std::exception& e){
-    //     cout<<e.what()<<endl;
-    // }
+    try{
+        string stock_data = string(argv[1]);
+        ifstream stocks(stock_data);
+        string line_stock;
+        vector<string> output_stock;
+        while (getline(stocks, line_stock)){
+            Helper::splitString(line_stock, output_stock, "|");
+            if(output_stock.size() >5){
+                throw std::invalid_argument("too many attributes given to build a stock object");
+            }else{
+                vector<string> given_price;
+                Helper::splitString(output_stock[3],given_price, ".");
+                if(given_price.size()>2){
+                    throw std::invalid_argument("Price had too many values when split");
+                }else{
+                    try{
+                        Price* p = new Price(stoi(given_price[0]), stoi(given_price[1]));
+                        Stock* stock = new Stock(output_stock[0],output_stock[1],output_stock[2],*p,std::make_unsigned_t<int>(stoi(output_stock[4])));
+                        Node* new_node = new Node(stock);
+                        all_stock->Append(new_node);
+                    }catch(std::exception& e){
+                        cout<<e.what()<<endl;
+                    }
+                }
+            }
+
+            output_stock.clear();
+        }
+        stocks.close();
+    }catch(std::exception& e){
+        cout<<e.what()<<endl;
+    }
+    int user_input =  main_menu_input();
+    // cout<<user_input<<endl;
+    //user input 9 is quit, and user input -1 is eof char
+    while (user_input !=9){
+        //main thingy here
+        cout<<"well done"<<endl;
+        return 0;
+    }
     
-    // /* read coin file*/
+    
+
+
+    /* read coin file*/
     // try{
     //     string coins_data = string(argv[2]);
     //     ifstream coins(coins_data);
@@ -100,8 +142,6 @@ int main(int argc, char **argv)
     //         if(temp.size() > 2){
     //             string exception_string = "the number of items in the line was not 2\n the line was: "+line_coins;
     //             throw std::invalid_argument(exception_string);
-    //         }else{
-                
     //         }
             
     //     }
@@ -114,9 +154,6 @@ int main(int argc, char **argv)
     
     // cout<<"finished reading files"<<endl;
     
-    Coin c(700,1);
-    cout<<c.denom<<c.count<<endl;
+    // return EXIT_SUCCESS;
 
-
-    return EXIT_SUCCESS;
 }
