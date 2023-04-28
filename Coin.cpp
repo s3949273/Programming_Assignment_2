aed7e55745b6e94197f8d11e911bd7fd83166b03
s3949273 <s3949273@student.rmit.edu.au>
2023-04-28 21:41:54 +1000

    i don't even know

--- Coin.cpp
+++ Coin.cpp
@@ -6,17 +6,19 @@ Coin::Coin(unsigned cents, unsigned amount){
     vector<int> denominations{5,10,20,50, 100, 200, 500, 1000};
     size_t index = 7;
     bool denom_found = false;   
+    this->count = amount;
     while (!denom_found && index >=0){
         if(cents % denominations[index]==0){
             //use of static_cast referenced from: https://stackoverflow.com/questions/321801/enum-c-get-by-index 
-            this->denom = static_cast<Denomination>(index);
-            denom_found = true;
+                this->denom = static_cast<Denomination>(index);
+                denom_found = true;
         }
         else{
             index -=1;
         }
     }
-    this->count = amount;
+    std::cout<<std::endl;
+    std::cout<<float(cents / denominations[index])<<std::endl;
 
 
 }
