75fb72f7ed21df36d843787551c67b11677c5bd2
s3949273 <s3949273@student.rmit.edu.au>
2023-05-05 15:53:30 +1000

    testing some new things

--- test.cpp
+++ test.cpp
@@ -8,19 +8,14 @@ using std::cout;
 using std::endl;
 using std::string;
 
+
+
 int main(int argc, char* argv[]){
     // int* a = new int(1);
     // int* b = new int(2);
     // cout<<Helper::strip_ID(string("0"))<<endl;
     
     LinkedList* ll = new LinkedList();
-    ll->pay(50,1);
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-    string input = Helper::readInput();
-    cout<<"Input is: "<<input<<endl;
-    // cout<<"hello"<<endl;
 
     // ll->open_stock_file(string(argv[1]));
     // 
@@ -28,13 +23,14 @@ int main(int argc, char* argv[]){
     // ll->add_item();
     // ll->display();
     // ll->add_item();
-    // Node* n = new Node(new Stock(string("I0001"), string("node"), string("1 node in linkedlist"), Price(1,1), 10));
-    // Node* n1 = new Node(new Stock(string("I0002"), string("node 1"), string("2 node in linkedlist"), Price(1,3), 10));
-    // Node* n2 = new Node(new Stock(string("I0003"), string("node 2"), string("3 node in linkedlist"), Price(1,4), 10));
-    // ll->append(n);
-    // ll->append(n1);
-    // ll->append(n2);
-    // ll->display();
+    Node* n = new Node(new Stock(string("I0001"), string("block"), string("1 node in linkedlist"), Price(1,1), 10));
+    Node* n1 = new Node(new Stock(string("I0002"), string("crit"), string("2 node in linkedlist"), Price(1,3), 10));
+    Node* n2 = new Node(new Stock(string("I0003"), string("attack"), string("3 node in linkedlist"), Price(1,4), 10));
+    ll->append(n);
+    ll->append(n1);
+    ll->append(n2);
+    // ll->test();
+    ll->display();
     delete ll;
     return 0;
 }
